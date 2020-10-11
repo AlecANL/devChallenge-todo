b00626210f97e0161e891eef55bc390072ca3f34
alec <alexeialvarez4@gmail.com>
2020-10-11 13:41:40 -0700

    add .gitignore

--- assets/js/classes/index.js
+++
@@ -1,4 +0,0 @@
-import { List } from './list.class.js';
-import { Todo } from './todo.class.js';
-
-export { List, Todo };
--- assets/js/classes/todo.class.js
+++
@@ -1,7 +0,0 @@
-export class Todo {
-  constructor(todo) {
-    this.id = new Date().getTime();
-    this.name = todo;
-    this.complete = false;
-  }
-}
